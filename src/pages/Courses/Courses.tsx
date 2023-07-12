64d789b886f4afa9d1f87c2554f701491d6cc3ce
thienminhkl <watermahou@gmail.com>
2023-07-12 18:13:18 +0700

    feature: Course list review

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -1,9 +1,52 @@
+import { Box, Stack, Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import CourseItem from '~/components/course-item/CourseItem';
+import { CYBER_TOKEN } from '~/const/const';
+import { Course, CourseList } from '~/type/course/course';
 import './Courses.scss';
 
 //---------------------------------------------------------------------
 
-function Courses() {
-  return <div>Courses</div>;
-}
+export default function Courses() {
+  const [listCourse, setListCourse] = useState<CourseList | null>([]);
 
-export default Courses;
+  const handleGetCourse = async () => {
+    try {
+      const resp = await axios({
+        url: 'https://elearningnew.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc_PhanTrang?page=1&pageSize=8&MaNhom=GP01',
+        method: 'get',
+        headers: { TokenCybersoft: ` ${CYBER_TOKEN}` },
+      });
+
+      setListCourse(resp.data.items);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetCourse();
+  }, []);
+
+  return (
+    <Stack m={3}>
+      <Typography variant="h4" sx={{ my: 2 }}>
+        Các khóa học mới nhất
+      </Typography>
+      <Box
+        gap={4}
+        display="grid"
+        gridTemplateColumns={{
+          xs: 'repeat(1, 1fr)',
+          sm: 'repeat(2, 1fr)',
+          md: 'repeat(4, 1fr)',
+        }}
+      >
+        {listCourse?.map((course: Course) => (
+          <CourseItem course={course} key={course?.maKhoaHoc} />
+        ))}
+      </Box>
+    </Stack>
+  );
+}
