8fa48f652715683fdd293c06d80f2ee980619b4c
Tran Binh Trieu <tranbinhtrieuhgbt2004@gmail.com>
2023-07-19 20:47:25 +0700

    bug: vercel

--- src/components/footers/admin/Footer.tsx
+++ src/components/footers/admin/Footer.tsx
@@ -1,5 +1,5 @@
 import { Stack } from '@mui/material';
-import Copyright from '~/components/copyright/Copyright';
+import Copyright from './Copyright';
 
 //---------------------------------------------------------------------------------
 
